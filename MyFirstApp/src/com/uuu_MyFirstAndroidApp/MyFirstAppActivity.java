ffcb2889a5f5722284c2d9df69913906de5a8c80
Administrator <Administrator@AndBiz>
2011-08-13 11:50:55 +0800

    first commit

---
+++ MyFirstApp/src/com/uuu_MyFirstAndroidApp/MyFirstAppActivity.java
@@ -0,0 +1,13 @@
+package com.uuu_MyFirstAndroidApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyFirstAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
